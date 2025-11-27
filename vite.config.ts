c0e61686bb98614c106fee32d3365ac38856e3d7
sorato0110 <zhuweikongdong@gmail.com>
2025-11-27 23:55:12 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
@@ -6,21 +7,19 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
 
   return {
-    // ★ GitHub Pages のサブフォルダに合わせる
+    // GitHub Pages のサブフォルダに合わせる
     base: '/-/',
 
-    server: {
-      port: 3000,
-      host: '0.0.0.0',
-    },
     plugins: [react()],
+
     define: {
       'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
+
     resolve: {
       alias: {
-        '@': path.resolve(__dirname, '.'),
+        '@': path.resolve(__dirname, './'),
       },
     },
   };
