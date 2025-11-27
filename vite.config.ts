bafa0c47c7bf1a9604cb038b6fd03ee9e2567dbb
sorato0110 <zhuweikongdong@gmail.com>
2025-11-27 23:58:40 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
@@ -7,7 +6,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
 
   return {
-    // GitHub Pages のサブフォルダに合わせる
+    // ★ GitHub Pages のサブフォルダに合わせる
     base: '/-/',
 
     plugins: [react()],
